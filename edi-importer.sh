490a5778bb966d13f49d307eae22586b001c2e71
Corey Rowe <coreyr1196@gmail.com>
2023-05-19 10:12:06 -0400

    Extract route list from GTFS archive

---
+++ edi-importer.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# edi-importer
+# Given a GTFS, generate textfiles compatible with the Eliot Deviation Index calculator.
+
+# Usage:
+# ./edi-importer [input GTFS archive] [output text file]
+# Default output file is "edi_import.txt"
+gtfs_archive="$1"
+output="$2"
+
+if [ -z $1 ]
+    then
+        echo "Please select a GTFS archive. Usage: ./edi-importer.sh INPUT_ARCHIVE OUTPUT_TEXT_FILE"
+	exit
+    fi
+
+echo "Routes found in GTFS archive:"
+
+unzip -p $1 routes.txt | cat
+
+echo
+read -p "Enter route to import (numerical entry only): " routevar
+
+unzip -p $1
+echo $routevar
+
